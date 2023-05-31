4eca8651865094fa4f755520a769b1c4a52ff6a9
MatasRutkauskas <matasrut00@gmail.com>
2023-05-31 20:37:06 +0300

    remove .env file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import * as admin from 'firebase-admin';
-import * as serviceAccount from '../firebase-service-key.json';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true }); // find out how to use specific cors url
@@ -19,15 +18,15 @@ async function bootstrap() {
     );
 
     const firebase_params = {
-        type: serviceAccount.type,
-        projectId: serviceAccount.project_id,
-        privateKey: serviceAccount.private_key,
-        client_email: serviceAccount.client_email,
-        clientId: serviceAccount.client_id,
-        authUri: serviceAccount.auth_uri,
-        tokenUri: serviceAccount.token_uri,
-        authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-        clientX509CertUrl: serviceAccount.client_x509_cert_url,
+        type: process.env.TYPE,
+        projectId: process.env.PROJECT_ID,
+        privateKey: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        clientId: process.env.CLIENT_ID,
+        authUri: process.env.AUTH_URI,
+        tokenUri: process.env.TOKEN_URI,
+        authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
+        clientX509CertUrl: process.env.CLIENT_X509_CERT_URL,
     };
 
     admin.initializeApp({
