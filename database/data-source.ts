915610343d804dc1ffabd2f120182b4aefe1854d
Ignas Kavaliauskas <i.kavaliauskas@amberlo.io>
2023-06-11 22:01:02 +0300

    create new initial migration, fix data seeding and dropping scripts, fix null ref error if company has no clients, add dotenv to be able to use env variables in script files

--- database/data-source.ts
+++ database/data-source.ts
@@ -1,12 +1,14 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
     host: process.env.PG_HOST,
-    port: +process.env.PGPORT,
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    port: +process.env.PG_PORT,
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE_NAME,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/database/migrations/*.js'],
 };
